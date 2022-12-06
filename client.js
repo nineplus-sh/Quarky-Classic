f146a9759558ad21f39915bc934c7fcf98f9524b
Vukky <vukky@litdevs.org>
2022-12-06 18:32:25 +0100

    return somewhat of the clieeeeeeeeeeeeent
    Co-authored-by: Skelly <vtheskeleton@users.noreply.github.com>

--- client.js
+++ client.js
@@ -1,34 +1,147 @@
+/**
+ * Resolve promise after set amount of ms
+ * @param {int} - ms to wait for
+ */
 const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+/**
+ * Get the value of a cookie
+ * @param {string} key 
+ * @returns {string} cookie value
+ */
 function getCookie(key) {
     var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
     return keyValue ? keyValue[2] : null;
 }
-if(!getCookie("authToken")) document.location.pathname = "/";
-window.authToken = getCookie("authToken");
 
+// Redirect to login screen if no token is presemt
+if(!getCookie("authToken")) document.location.pathname = "/";
+const authToken = getCookie("authToken");
+
+// Quarks
+let quarks = {};
+
+// Websocket related variables
+let wss; // Actually the websocket
+let retryCount = 0; // Amount of connection retries
+let welcomeHasFinishedOnce = false; // Keep track of if the welcome flow has finished once
+let heartbeat;
+
+/**
+ * Initialize Quarky
+ * @returns {void}
+ */
 async function welcome() {
+    changeLoading("Opening gateway connection...")
+    openGateway()
     changeLoading("Fetching Quarks...");
-    window.serverSocket = new WebSocket("wss://lq-gateway.litdevs.org", authToken);
+    quarks = await quarkFetch();
+    quarkRender(quarks);
+    changeLoading("Doing the epic transition...");
+    document.querySelector("#loader").classList.add("bye");
+    new Audio("/assets/sfx/wb.mp3").play();
+    welcomeHasFinishedOnce = true;
 }
+
+/**
+ * Change loading screen text.
+ * @param {string} text 
+ * @returns {void}
+ */
 function changeLoading(text) {
-    document.querySelector("#loaderexp").innerText = text;
+    document.querySelector("#loaderexp").innerHTML = text;
 }
-async function quarkRender() {
-    return new Promise(function(resolve, reject) {
-        
+
+let quarkTip;
+/**
+ * Populate quark list and add join & log out buttons
+ * Clears old list
+ * @param {object} quarks - List of quarks to populate the list with
+ * @returns {void}
+ */
+async function quarkRender(quarks) { // i mean.. that only happens once? yeah true
+    let quarkList = document.querySelector("#list");
+    quarkList.innerHTML = "";
+    quarks.forEach(quark => { // ok i wonder if this actually works it should
+        quarkList.innerHTML += `<div class="quark" id="${quark._id}" onclick="switchQuark('${quark._id}');" data-tippy-content="${quark.name}">
+    <img src="${quark.iconUri}">
+</div>`
+    })
+    // Add join and log out buttons
+    quarkList.innerHTML += `
+            <div class="quark joiner" onclick="joinQuark();" data-tippy-content="Join a Quark">
+                <span style="font-size: 2.88em; margin-left: 0.4em;">+</span>
+            </div>
+            <div class="quark logout" onclick="logOut();" data-tippy-content="Log Out :(">
+                <span style="font-size: 2.4em; margin-left: 0.4em;">←</span>
+            </div>`
+    // Create a tippy tooltip if it doesnt already exist
+    if (quarkTip) quarkTip.destroy();
+    quarkTip = tippy(`.quark`, { 
+        placement: "right",
+        theme: "black"
     });
 }
+/**
+ * Get the user's quarks
+ * @returns {object} - Quark array
+ */
 async function quarkFetch() {
-    return new Promise(function(resolve, reject) {
-        
-    });
+    let quarkResponse = await apiCall("/quark/me");
+    if (quarkResponse) return quarkResponse.response.quarks;
 }
-async function api(url, data = null, method = "GET") {
-    return new Promise(function(resolve, reject) {
-        fetch(url, {"headers": {"Authentication": `Bearer ${authToken}`, "body": data, "method": method}})
-            .then(response => response.json())
-            .then(response => resolve(response))
-            .catch(error => reject(error))
-    });
+
+/**
+ * Make call to Lightquark API.
+ * Returns false if failed, otherwise returns data.
+ * 
+ * @param {string} path  - Api endpoint, `/quark/me`
+ * @param {"GET" | "POST" | "PATCH" | "PUT" | "DELETE"} method - Default GET
+ * @param {object} body - Default empty object
+ * @returns {object|false}
+ */
+ async function apiCall(path, method = "GET", body = {}) {
+    let options = {
+        method: method,
+        headers: {
+            "Authorization": `Bearer ${authToken}`,
+            "Content-Type": "application/json"
+        }
+    }
+    // GET requests cannot have a body
+    if (method !== "GET") options.body = JSON.stringify(body);
+
+    try {
+        let res = await fetch(`https://lq.litdevs.org/v1${path}`, options);
+        let data = await res.json();
+        if (data.request.success) return data; // Success
+        if (data.request.status_code === 401)  {
+            logOut();
+            return logOut(); // this isn't a good idea right
+        }
+        // Failed :(
+        alert(`${data.request.status_code}:\n${data.response.message}`)
+        return false;
+    } catch (e) {
+        alert(`Failed to make API call:\n${e}`);
+        return false;
+    }
 }
+
+function logOut() {
+    document.cookie = "authToken=";
+    document.location.pathname = "/";
+}
+
+function joinQuark() {
+    let quarkCode = prompt("Enter the invite code for the Quark you want to join.");
+    alert("Unfortunately, I'm not gonna do anything with that information :)")
+}
+
+function switchQuark(id) {
+    document.querySelector(`.quark[id='${id}']`).classList.remove("stretch");
+    void document.querySelector(`.quark[id='${id}']`).offsetWidth;
+    document.querySelector(`.quark[id='${id}']`).classList.add("stretch");
+}
+
 welcome();
---
+++ gateway.js
@@ -0,0 +1,111 @@
+/**
+ * Open gateway connection
+ * Call socketListeners() to add listeners
+ */
+function openGateway() {
+    wss = new WebSocket("wss://lq-gateway.litdevs.org", authToken);
+    socketListeners();
+}
+
+/**
+ * Adds listeners to gateway socket
+ */
+function socketListeners() {
+    wss.onopen = (message) => {
+        retryCount = 0; // Connection open, reset retry counter
+        // If the welcome flow has been completed before, just hide the loader
+        if (welcomeHasFinishedOnce) {
+            document.querySelector("#loader").classList.add("bye");
+        }
+        // Send heartbeat to server every 15 seconds
+        heartbeat = setInterval(() => {
+            console.log(saLines[saIndex]) // i need to know if it's doin the funnies
+           wss.send(JSON.stringify({event: "heartbeat", message: saLines[saIndex]}))
+           saIndex += 1;
+           if(saIndex === saLines.length - 1) saIndex = 0;
+        }, 15000);
+    }
+    wss.onmessage =(message) => {
+        // stuff
+    }
+    wss.onclose = (message) => {
+        if (heartbeat) clearInterval(heartbeat);
+        document.querySelector("#loader").classList.remove("bye");
+        changeLoading("Connection lost<br/>Please wait - attempting to reestablish...");
+        if (retryCount < 5) { // Max 5 retries
+            // Try reconnect after 1*retryCount seconds
+            retryCount++;
+            setTimeout(() => {
+                openGateway();
+            }, 1000 * retryCount);
+        } else {
+            alert("Can't open gateway connection, either your internet connection is broken, server is down or your login is wrong");
+        }
+    }
+
+    wss.onerror = (message) => {
+        // stuff 3
+    }
+}
+
+let saLines = `This was a triumph
+I'm making a note here; "Huge success"
+It's hard to overstate
+My satisfaction
+Aperture Science:
+We do what we must
+Because we can
+For the good of all of us
+Except the ones who are dead
+But there's no sense crying
+Over every mistake
+You just keep on trying
+Till you run out of cake
+And the science gets done
+And you make a neat gun
+For the people who are
+Still alive
+I'm not even angry
+I'm being so sincere right now
+Even though you broke my heart,
+And killed me
+And tore me to pieces
+And threw every piece into a fire
+As they burned it hurt because
+I was so happy for you
+Now, these points of data
+Make a beautiful line
+And we're out of beta
+We're releasing on time
+So I'm GLaD I got burned
+Think of all the things we learned-
+For the people who are
+Still alive
+Go ahead and leave me
+I think I'd prefer to stay inside
+Maybe you'll find someone else
+To help you?
+Maybe Black Mesa?
+That was a joke *Haha - Fat Chance*
+Anyway this cake is great
+It's so delicious and moist
+Look at me: still talking
+When there's science to do
+When I look out there,
+It makes me GLaD I'm not you
+I've experiments to run
+There is research to be done
+On the people who are
+Still alive
+And believe me I am
+Still alive
+I'm doing science and I'm
+Still alive
+I feel fantastic and I'm
+Still alive
+While you're dying I'll be
+Still alive
+And when you're dead I will be
+Still alive
+Still alive`.split("\n");
+let saIndex = 0;
