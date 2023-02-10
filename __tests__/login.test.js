9ae2965ed4059f8be7bab3dda0cc37f8053cb50d
Vukky <vukky@litdevs.org>
2023-02-10 21:55:48 +0100

    Quarker

---
+++ __tests__/login.test.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+beforeAll(async () => {
+    await page.goto('http://127.0.0.1:4210');
+    await page.setDefaultTimeout(2000);
+});
+
+describe('the login handler', () => {
+    it('should deny nonexistent accounts', async () => {
+        await expect(page).toFillForm('form[id="credentials"]', {
+            email: "a@a",
+            password: "pæsswørd",
+        });
+        await expect(page).toClick("button", { text: "Done :)" });
+        await expect(page).toMatchElement('#email[class="fail"]');
+    });
+    it('should deny incorrect passwords', async () => {
+        await expect(page).toFillForm('form[id="credentials"]', {
+            email: process.env.LITAUTH_EMAIL,
+            password: "pæsswørd",
+        });
+        await expect(page).toClick("button", { text: "Done :)" });
+        await expect(page).toMatchElement('#password[class="fail"]');
+    });
+    it('should redirect valid login details', async () => {
+        await expect(page).toFillForm('form[id="credentials"]', {
+            email: process.env.LITAUTH_EMAIL,
+            password: process.env.LITAUTH_PASSWORD,
+        });
+        await expect(page).toClick("button", { text: "Done :)" });
+        await expect(page).toMatchElement('#planet[class="moveplanet"]');
+        await page.reload();
+        await expect(page.url()).toMatch('http://127.0.0.1:4210/client.html');
+    });
+});
