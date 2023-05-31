d65cb9e7cf06cbdd3772e9df9ce0ae016f2bca26
Vukky B. Tukky Fukky <vukky@litdevs.org>
2023-05-31 17:56:57 +0200

    Vukky caught using feature gacha
    Holy hell

--- public/assets/vukkybox.js
+++ public/assets/vukkybox.js
@@ -4,15 +4,18 @@
  *  @param {string} image - The Vukky asset to use.
  *  @param {string} title - The title.
  */
-function vukkybox(message, image, title) {
+async function vukkybox(message, image, title) {
     let vickyboxen = document.createElement("dialog");
     if(image) vickyboxen.innerHTML += `<object data="/assets/img/${image}.svg" style="float: left; padding-right: 1em">`;
     if(title) vickyboxen.innerHTML += `<h2 style="margin: 0;">${title}</h2>`;
     if(message) vickyboxen.innerHTML += message;
-    vickyboxen.innerHTML += "<hr><div style='text-align: center'><button>Ok</button></div>";
+    vickyboxen.innerHTML += "<hr><form method='dialog'><div style='text-align: center'><button>Ok</button></div></form>";
     if(window.userAvatar && image) vickyboxen.querySelector("object").onload = function() {
             this.contentDocument.querySelectorAll(".loadpfp").forEach(doodad => doodad.setAttribute("xlink:href", window.userAvatar))
     }
+    vickyboxen.addEventListener("close", (e) => {
+        return vickyboxen.returnValue;
+    })
     document.body.appendChild(vickyboxen);
     vickyboxen.showModal()
  }
