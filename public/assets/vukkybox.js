e5db0af040a1cbb2185296dec59509e50144a184
Vukky <vukky@litdevs.org>
2023-05-31 16:57:43 +0200

    fix pfp not showing in the portal.svg

--- public/assets/vukkybox.js
+++ public/assets/vukkybox.js
@@ -1,15 +1,18 @@
 /**
  * Spawns a Vukkybox.
  *  @param {string} message - The message.
- *  @param {string} image - The Vukky sprite to use.
+ *  @param {string} image - The Vukky asset to use.
  *  @param {string} title - The title.
  */
 function vukkybox(message, image, title) {
     let vickyboxen = document.createElement("dialog");
-    if(image) vickyboxen.innerHTML += `<img src="/assets/img/${image}" style="float: left; padding-right: 1em">`;
+    if(image) vickyboxen.innerHTML += `<object data="/assets/img/${image}.svg" style="float: left; padding-right: 1em">`;
     if(title) vickyboxen.innerHTML += `<h2 style="margin: 0;">${title}</h2>`;
     if(message) vickyboxen.innerHTML += message;
-    vickyboxen.innerHTML += "<hr><div style='text-align: center'><button>Ok</button></div>"
+    vickyboxen.innerHTML += "<hr><div style='text-align: center'><button>Ok</button></div>";
+    if(window.userAvatar && image) vickyboxen.querySelector("object").onload = function() {
+            this.contentDocument.querySelectorAll(".loadpfp").forEach(doodad => doodad.setAttribute("xlink:href", window.userAvatar))
+    }
     document.body.appendChild(vickyboxen);
     vickyboxen.showModal()
  }
